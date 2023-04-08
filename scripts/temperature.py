09f8fc9a3d64f24a0f4214375cf1d410b2919086
caiquemiranda <caiquemirandas@gmail.com>
2023-04-08 05:25:28 -0300

    temperature

---
+++ scripts/temperature.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+fahrenheit = 0.0
+print("Fahrenheit Celsius")
+while fahrenheit <= 250:
+    celsius = (fahrenheit - 32.0) / 1.8
+    print(f'{fahrenheit}, {celsius}')
+    fahrenheit = fahrenheit + 25
+    
